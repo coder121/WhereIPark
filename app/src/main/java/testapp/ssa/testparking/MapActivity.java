4d95cfb540facc0cd6fb3a9ecde3fc90d6add0a1
siddiq <siddiq235@gmail.com>
2015-12-23 19:18:37 -0800

    added google map markers on current location and parking

--- app/src/main/java/testapp/ssa/testparking/MapActivity.java
+++ app/src/main/java/testapp/ssa/testparking/MapActivity.java
@@ -1,6 +1,14 @@
 package testapp.ssa.testparking;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import android.os.Bundle;
@@ -12,19 +20,38 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private double lat;
     private double longi;
+    LocationManager locationManager;
+    private LatLng currentLoc;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+        currentLoc = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
         Bundle bundle = getIntent().getExtras();
-        lat=bundle.getDouble("lat");
-        longi=bundle.getDouble("longi");
+        lat = bundle.getDouble("lat");
+        longi = bundle.getDouble("longi");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -33,9 +60,20 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap map) {
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng carPark = new LatLng(lat, longi);
-        map.addMarker(new MarkerOptions().position(carPark).title("Parking"));
+        map.setMyLocationEnabled(true);
+//
 
+
+        LatLng carPark = new LatLng(lat, longi);
+//        LatLng carPark = new LatLng(37.22, -121.80);
+        map.addMarker(new MarkerOptions().position(carPark).title("Parking"));
+//        map.addMarker(currentLocationMarker);
+
+        map.addMarker(new MarkerOptions().position(currentLoc).title("MyLocation"));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(carPark, (float) 16.0));
+        Polyline line = map.addPolyline(new PolylineOptions()
+                .add(carPark, currentLoc)
+                .width(5)
+                .color(Color.RED));
     }
 }
